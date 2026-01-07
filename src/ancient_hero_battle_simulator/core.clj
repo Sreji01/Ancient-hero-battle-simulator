89f5c374afc82e204b164c28c01475d380c22aaa
Sreji01 <marko.srejic01@gmail.com>
2026-01-07 23:42:23 +0100

    Created a simple console app and added heroes.clj

--- src/ancient_hero_battle_simulator/core.clj
+++ src/ancient_hero_battle_simulator/core.clj
@@ -1,7 +1,19 @@
 (ns ancient-hero-battle-simulator.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn show-menu []
+  (println "1. Select heroes for combat")
+  (println "2. Create your hero")
+  (println "3. Exit"))
+
+(defn -main []
+  (loop []
+    (show-menu)
+    (let [choice (read-line)]
+      (case choice
+        "1" (println "Select heroes...")
+        "2" (println "Create your hero...")
+        "3" (println "Bye!")
+        (println "Invalid choice"))
+      (when-not (= choice "3")
+        (recur)))))
---
+++ src/ancient_hero_battle_simulator/heroes.clj
@@ -0,0 +1,25 @@
+(ns ancient-hero-battle-simulator.heroes)
+
+(def achilles
+  {:id 1
+   :name "Achilles"
+   :title "Legendary Warrior of Troy"
+
+   :stats
+   {:health 85
+    :power 90
+    :defense 70
+    :agility 100
+    :intelligence 80}})
+
+(def king-arthur
+  {:id 2
+   :name "King Arthur"
+   :title "The Once and Future King"
+
+   :stats
+   {:health 90
+    :power 80
+    :defense 85
+    :agility 70
+    :intelligence 90}})
