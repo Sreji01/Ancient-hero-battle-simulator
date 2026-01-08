36d7a8bdeeda204d64bc1a6976fbdccd8bf6de98
Sreji01 <marko.srejic01@gmail.com>
2026-01-08 21:06:30 +0100

    Added heroes vector and extended menu options.

--- src/ancient_hero_battle_simulator/core.clj
+++ src/ancient_hero_battle_simulator/core.clj
@@ -1,19 +1,37 @@
 (ns ancient-hero-battle-simulator.core
   (:gen-class))
 
-(defn show-menu []
+(defn show-main-menu []
+  (println "\nMain Menu")
   (println "1. Select heroes for combat")
   (println "2. Create your hero")
   (println "3. Exit"))
 
-(defn -main []
+(defn show-combat-menu []
+  (println "\nSelect combat type")
+  (println "1. 1v1")
+  (println "2. 2v2")
+  (println "3. 3v3")
+  (println "4. Back to main menu"))
+
+(defn select-combat []
   (loop []
-    (show-menu)
+    (show-combat-menu)
     (let [choice (read-line)]
       (case choice
-        "1" (println "Select heroes...")
-        "2" (println "Create your hero...")
+        "1" (do (println "You chose 1v1!") (recur))
+        "2" (do (println "You chose 2v2!") (recur))
+        "3" (do (println "You chose 3v3!") (recur))
+        "4" (println "Returning to main menu...")
+        (do (println "Invalid choice") (recur))))))
+
+
+(defn -main []
+  (loop []
+    (show-main-menu)
+    (let [choice (read-line)]
+      (case choice
+        "1" (do (select-combat) (recur))
+        "2" (do (println "Create your hero...") (recur))
         "3" (println "Bye!")
-        (println "Invalid choice"))
-      (when-not (= choice "3")
-        (recur)))))
+        (do (println "Invalid choice") (recur))))))
--- src/ancient_hero_battle_simulator/heroes.clj
+++ src/ancient_hero_battle_simulator/heroes.clj
@@ -7,7 +7,7 @@
 
    :stats
    {:health 85
-    :power 90
+    :power 95
     :defense 70
     :agility 100
     :intelligence 80}})
@@ -23,3 +23,59 @@
     :defense 85
     :agility 70
     :intelligence 90}})
+
+(def hercules
+  {:id 3
+   :name "Hercules"
+   :title "Son of Zeus"
+
+   :stats
+   {:health 100
+    :power 100
+    :defense 80
+    :agility 65
+    :intelligence 70}})
+
+(def beowulf
+  {:id 4
+   :name "Beowulf"
+   :title "Slayer of Monsters"
+
+   :stats
+   {:health 95
+    :power 85
+    :defense 90
+    :agility 60
+    :intelligence 75}})
+
+(def leonidas
+  {:id 5
+   :name "Leonidas"
+   :title "King of Sparta"
+
+   :stats
+   {:health 88
+    :power 85
+    :defense 95
+    :agility 75
+    :intelligence 80}})
+
+(def gilgamesh
+  {:id 6
+   :name "Gilgamesh"
+   :title "King of Uruk"
+
+   :stats
+   {:health 92
+    :power 88
+    :defense 82
+    :agility 78
+    :intelligence 85}})
+
+(def heroes
+  [achilles
+   king-arthur
+   hercules
+   beowulf
+   leonidas
+   gilgamesh])
