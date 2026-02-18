77d8abac9c89f0bdf7465aab6add65c2c44cd1ac
Sreji01 <marko.srejic01@gmail.com>
2026-02-18 15:05:27 +0100

    Refactored logic file into separated files.

---
+++ src/ancient_hero_battle_simulator/game/combat_logic.clj
@@ -0,0 +1,57 @@
+(ns ancient-hero-battle-simulator.game.combat-logic
+  (:require [ancient-hero-battle-simulator.game.ui :as ui]
+            [ancient-hero-battle-simulator.game.game-state :as state]
+            [ancient-hero-battle-simulator.game.utilility :as util]))
+
+(defn calculate-hit? [attacker target]
+  (let [atk-stats @(:current-stats attacker)
+        tar-stats @(:current-stats target)
+        hit-chance (+ 80 (* (:intelligence atk-stats) 0.1)
+                      (* (- (:agility tar-stats)) 0.15))
+        evade-chance (or (:evade tar-stats) 0)
+        roll (rand-int 100)]
+    (cond
+      (< roll evade-chance) :dodge-roll
+      (> (rand-int 100) hit-chance) :dodge
+      :else :hit)))
+
+(defn compute-damage [attacker target]
+  (let [atk-stats @(:current-stats attacker)
+        tar-stats @(:current-stats target)
+        raw-damage (:power atk-stats)
+        reduction (* (:defense tar-stats 0) 0.5)
+        damage-reduction (:damage-reduction tar-stats 0)]
+    (int (max 5 (- raw-damage reduction damage-reduction)))))
+
+(defn apply-damage! [target target-player-hp damage]
+  (swap! (:current-hp target) #(max 0 (- % damage)))
+  (swap! target-player-hp #(max 0 (- % damage))))
+
+(defn attack! [attacker target target-player-hp enemy-field]
+  (let [outcome (calculate-hit? attacker target)
+        damage  (if (= outcome :hit)
+                  (compute-damage attacker target)
+                  0)]
+    (when (= outcome :dodge-roll)
+      (swap! (:current-stats target) dissoc :evade))
+
+    (when (= outcome :hit)
+      (apply-damage! target target-player-hp damage)
+      (state/check-and-remove-dead! target enemy-field)
+
+      (let [stats-atom (:current-stats target)]
+        (when (> (:damage-reduction @stats-atom 0) 0)
+          (swap! stats-atom assoc :damage-reduction 0))))
+
+    (ui/print-attack-message attacker target outcome damage)))
+
+(defn perform-attack [player-name attacker player-field enemy-field enemy-player-hp]
+  (if (:skip-attack? attacker)
+    (do
+      (println (str "\n" (:name attacker) " is forced to skip their attack!"))
+      (state/update-hero-on-field! player-field (dissoc attacker :skip-attack?)))
+    (let [targets (state/heroes-on-field @enemy-field)
+          target  (util/choose-hero targets
+                               (if (= player-name "BLUE") "Red" "Blue")
+                               "to attack")]
+      (attack! attacker target enemy-player-hp enemy-field))))
